9f0f1ebc4d5af6fd52593be69ddec6132fb04c94
Andre Filho <andre.filho001@outlook.com>
2018-12-03 13:15:46 -0200

    CI: make docker run but still not shown in browser

--- Dockerfile
+++ Dockerfile
@@ -4,10 +4,8 @@ RUN apt-get update
 
 WORKDIR /smi-slave
 
-COPY . /smi-slave
+ADD . /smi-slave
 
 RUN pip install --no-cache-dir -r requirements.txt
-
-ENV FLASK_ENV="docker"
 
 EXPOSE 5000
--- src/app.py
+++ src/app.py
@@ -1,6 +1,11 @@
 from flask import Flask
 app = Flask(__name__)
 
+if __name__ == '__main__':
+  app.run(debug=True, host='0.0.0.0')
+
+
 @app.route("/")
 def hello():
   return "Hello World!"
+
