f48d5566aac4c7e027f3ed016d8a2625afd2b36f
Andre de Sousa Costa Filho <andre.filho001@outlook.com>
2019-01-22 16:19:09 -0200

    Adds tests about transductor_model creation
    Signed-off-by: Guilherme Augusto <guilherme.francais@gmail.com>
    Signed-off-by: André Filho <andre.filho001@outlook.com>

--- smi/settings.py
+++ smi/settings.py
@@ -40,7 +40,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'transductor_model'
+    'transductor_model',
+    'transductor',
+    'rest_framework'
 ]
 
 MIDDLEWARE = [
