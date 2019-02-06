43232006e0f3930a2a5a974b1c6b0fff62c021ec
Guilherme AugustoD <guilherme.francais@gmail.com>
2019-02-06 14:09:02 -0200

    Modifies data collection to all collection types

--- data_reader/utils.py
+++ data_reader/utils.py
@@ -32,10 +32,24 @@ class DataCollector(object):
     def __init__(self):
         # self.transductors = Transductor.objects.filter(active=True)
         self.transductors = EnergyTransductor.objects.all()
+        self.functions_dict = build_functions_dict()
+        self.communications_dict = build_communications_dict()
 
-    # isso vai virar 3 pra salvar cada classe la
-    #
-    def single_data_collection(self, transductor):
+    def build_functions_map(self):
+        return {
+            "Minutely": minutely_data_collection,
+            "Quarterly": quartely_data_collection,
+            "Monthly": monthly_data_collection
+        }
+
+    def build_communication_dict(self):
+        return {
+            "Minutely": minutely_communication,
+            "Quarterly": quartely_communication,
+            "Monthly": monthly_communication
+        }
+
+    def single_data_collection(self, transductor, collection_type):
         """
         Thread method responsible to handle all the communication used
         by a transductor and save the measurements collected.
@@ -47,24 +61,13 @@ class DataCollector(object):
             None
         """
 
-        # Creating instances of the serial and transport protocol used
-        # by the transductor
-        serial_protocol_instance = globals()[
-            transductor.model.serial_protocol
-        ](transductor)
-        tranport_protocol_instance = globals()[
-            transductor.model.transport_protocol
-        ](serial_protocol_instance)
+        serial_protocol_instance, \
+        transport_protocol_instance = get_protocols(transductor)
 
-        try:
-            messages = tranport_protocol_instance.start_communication()
-        except (NumberOfAttempsReachedException, CRCInvalidException) as e:
-            if not transductor.broken:
-                transductor.set_broken(True)
-            return None
-
-        if transductor.broken:
-            transductor.set_broken(False)
+        messages, transductor = self.communications_dict[collection_type](
+            (serial_protocol_instance, transport_protocol_instance),
+            transductor
+        )
 
         measurements = []
 
@@ -74,46 +77,63 @@ class DataCollector(object):
                 .get_measurement_value_from_response(message)
             )
 
-        if transductor.model.name == "TR4020":
-            # aqui mudar para o save da classe la
-            EnergyMeasurement.save_measurements(measurements, transductor)
-        else:
-            pass
+        self.functions_dict[collection_type](measurements, transductor)
 
-    def minutely_data_collection(self, transductor):
-        # Creating instances of the serial and transport protocol used
-        # by the transductor
-        serial_protocol_instance = globals()[
-            transductor.model.serial_protocol
-        ](transductor)
-        tranport_protocol_instance = globals()[
-            transductor.model.transport_protocol
-        ](serial_protocol_instance)
-
-        try:
-            messages = tranport_protocol_instance.start_communication()
-        except (NumberOfAttempsReachedException, CRCInvalidException) as e:
-            if not transductor.broken:
-                transductor.set_broken(True)
-            return None
-
-        if transductor.broken:
-            transductor.set_broken(False)
-
-        measurements = []
-
-        for message in messages:
-            measurements.append(
-                serial_protocol_instance
-                .get_measurement_value_from_response(message)
-            )
-
+    def minutely_data_collection(self, measurements, transductor):
         if transductor.model.name == "TR4020":
             MinutelyMeasurement.save_measurements(measurements, transductor)
         else:
             pass
 
-    def perform_data_collection(self):
+    def quarterly_data_collection(self, measurements, transductor):
+        if transductor.model.name == "TR4020":
+            QuarterlyMeasurement.save_measurements(measurements, transductor)
+        else:
+            pass
+
+    def monthly_data_collection(self, measurements, transductor):
+        if transductor.model.name == "TR4020":
+            MonthlyMeasurement.save_measurements(measurements, transductor)
+        else:
+            pass
+
+    def get_protocols(self, transductor):
+        # Creating instances of the serial and transport protocol used
+        # by the transductor
+        serial_protocol_instance = globals()[
+            transductor.model.serial_protocol
+        ](transductor)
+        transport_protocol_instance = globals()[
+            transductor.model.transport_protocol
+        ](serial_protocol_instance)
+
+        return (serial_protocol_instance, transport_protocol_instance)
+
+    def minutely_communication(self, protocols, transductor):
+
+        serial_protocol_instance, \
+        transport_protocol_instance = protocols
+
+        registers = transductor.model.minutely_register_address
+
+        messages = []
+
+        try:
+            messages = \
+                tranport_protocol_instance.start_communication(
+                    registers
+                )
+        except (NumberOfAttempsReachedException, CRCInvalidException) as e:
+            if not transductor.broken:
+                transductor.set_broken(True)
+            return None
+
+        if transductor.broken:
+            transductor.set_broken(False)
+
+        return (messages, transductor)
+
+    def perform_all_data_collection(self, collection_type):
         """
         Method responsible to start all transductors data collection
         simultaneously.
@@ -125,7 +145,7 @@ class DataCollector(object):
 
         for transductor in self.transductors:
             collection_thread = Thread(
-                target=self.single_data_collection, args=(transductor,)
+                target=self.single_data_collection, args=(transductor, collection_type)
             )
 
             collection_thread.start()
