457e2b594d0a4cbb548f8055c60d9c6efe0eb6a2
Andre Filho <andre.filho001@outlook.com>
2019-03-11 10:42:09 -0300

    Fix cronjob problem affecting the runserver

--- scripts/start.sh
+++ scripts/start.sh
@@ -11,12 +11,15 @@ done;
 
 echo '======= RUNNING PIP INSTALL'
 pip install --no-cache-dir -r requirements.txt
+
 echo '======= MAKING MIGRATIONS'
 python3 manage.py makemigrations
+
 echo '======= RUNNING MIGRATIONS'
 python3 manage.py migrate
+
+echo '======= STARTING CRON'
+cron
+
 echo '======= RUNNING SERVER'
-
-cron -f
-
 python3 manage.py runserver 0.0.0.0:8000
