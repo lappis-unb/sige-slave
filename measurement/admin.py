852541c20fe67ab610ab1867fe5ed402c1386186
Andre Filho <andre.filho001@outlook.com>
2019-03-18 14:49:49 -0300

    Adds models to djangoadmin

--- measurement/admin.py
+++ measurement/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import MinutelyMeasurement
+from .models import QuarterlyMeasurement
+from .models import MonthlyMeasurement
 
-# Register your models here.
+
+admin.site.register(MinutelyMeasurement)
+admin.site.register(QuarterlyMeasurement)
+admin.site.register(MonthlyMeasurement)
