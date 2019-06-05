c775e3fa565123cb2e83e98244e245abfcc694d4
Andre Filho <andre.filho001@outlook.com>
2019-06-05 16:04:20 -0300

    Changeschanges error

--- smi/master_middleware.py
+++ smi/master_middleware.py
@@ -1,6 +1,7 @@
 import json as js
 import jwt as JWT
 from django.conf import LazySettings
+from django.http import HttpRequest
 from rest_framework.response import Response
 
 
@@ -23,13 +24,28 @@ class CommsMiddleware(object):
         jwt = JWT.JWT()
         jason = js.loads(request.body)
         a = jason['msg']
+
+        print('-=-=-=-=-=-=-=-=-=-=-')
         print(settings.SECRET_KEY)
+        print('-=-=-=-=-=-=-=-=-=-=-')
+
         key = JWT.jwk.OctetJWK(key=settings.SECRET_KEY.encode('utf-8'), kid=1)
         decoded_json = jwt.decode(a, key)
         new_body = js.dumps(decoded_json)
-        print(new_body)
-        request.update(body=new_body)
-        # request.body = new_body
 
-        response = self.get_response(request)
+        print('-=-=-=-=-=-=-=-=-=-=-')
+        print(new_body)
+        print('-=-=-=-=-=-=-=-=-=-=-')
+
+        new_request = HttpRequest()
+        new_request.POST = new_body
+        new_request.META.update(request.META)
+
+        print('-=-=-=-=-=-=-=-=-=-=-')
+        print('HUASHUASHUASASHUAHSUASU')
+        print(new_request.POST)
+        print('-=-=-=-=-=-=-=-=-=-=-')
+
+
+        response = self.get_response(new_request)
         return response
