81cc7620a6662612b62fd30dd2e675e8d9df1aa3
Guilherme Augusto <guilherme.francais@gmail.com>
2019-06-26 18:26:12 -0300

    Configures pagination details

---
+++ measurement/pagination.py
@@ -0,0 +1,11 @@
+from rest_framework.pagination import LimitOffsetPagination
+from rest_framework.pagination import PageNumberPagination
+
+
+class PostLimitOffsetPagination(LimitOffsetPagination):
+    default_limit = 50
+    max_limit = 100
+
+
+class PostPageNumberPagination(PageNumberPagination):
+    page_size = 5
