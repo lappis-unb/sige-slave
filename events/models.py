b8732c2409d9c1650594f1af2c8db5e7e0fda6e5
Andre Filho <andre.filho001@outlook.com>
2019-12-16 09:49:11 -0300

    Adds initial event and failed communication event classes

--- events/models.py
+++ events/models.py
@@ -1,3 +1,74 @@
 from django.db import models
+from django.conf import settings
+from django.utils import timezone
+from transductor.models import EnergyTransductor
+from django.core.validators import RegexValidator
 
-# Create your models here.
+
+class Event(models.Model):
+    """
+    Defines a new event object
+    """
+    settings.USE_TZ = False
+    created_at = models.DateTimeField(default=timezone.now)
+
+    def __format_data(self, data):
+        """
+        Takes the data of what created the event and saves it in a dict.
+        """
+        formatted_data = dict(
+
+        )
+        return formatted_data
+
+    def save_event(self):
+        """
+        Saves the event
+        """
+        raise NotImplementedError
+
+
+class FailedConnectionEvent(Event):
+    """
+    Defines a new event related to a failed connection with a transductor
+    """
+    transductor_ip = models.CharField(
+        max_length=15,
+        unique=True,
+        default="0.0.0.0",
+        validators=[
+            RegexValidator(
+                regex='^(?:[0-9]{1,3}\.){3}[0-9]{1,3}$',
+                message='Incorrect IP address format',
+                code='invalid_ip_address'
+            ),
+        ])
+    transductor = models.ForeignKey(
+        EnergyTransductor,
+        related_name="%(app_label)s_%(class)s",
+        on_delete=models.CASCADE,
+        blank=False,
+        null=False
+    )
+
+    @staticmethod
+    def save_event(transductor):
+        """
+        Saves a failed connection event related to a transductor
+        """
+        new_event = FailedConnectionEvent()
+        new_event.transductor = transductor
+        new_event.transductor_ip = new_event.transductor.ip_address
+
+        new_event.save()
+
+
+class CriticalVoltageEvent(Event):
+    """
+    Defines a new event related to a critical voltage measurement
+    """
+    # measurement =
+
+    @staticmethod
+    def save_event(critical_measures):
+        pass
