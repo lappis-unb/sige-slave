c46572d79143dd76639c165cd41a28916dfa0203
Guilherme Augusto <guilherme.francais@gmail.com>
2019-12-21 12:47:07 -0300

    Modifies events structure

--- events/models.py
+++ events/models.py
@@ -3,14 +3,26 @@ from django.core.validators import RegexValidator
 from django.db import models
 from django.utils import timezone
 from transductor.models import EnergyTransductor
+from polymorphic.models import PolymorphicModel
+from django.contrib.postgres.fields import JSONField
+from django.contrib.postgres.fields import ArrayField
 
 
-class Event(models.Model):
+class Event(PolymorphicModel):
     """
     Defines a new event object
     """
     settings.USE_TZ = False
     created_at = models.DateTimeField(default=timezone.now)
+    measures = JSONField()
+
+    transductor = models.ForeignKey(
+        EnergyTransductor,
+        related_name="%(app_label)s_%(class)s",
+        on_delete=models.CASCADE,
+        blank=False,
+        null=False
+    )
 
     def __str__(self):
         return '%s@%s' % (self.__class__.__name__, self.created_at)
@@ -22,75 +34,69 @@ class Event(models.Model):
         raise NotImplementedError
 
 
-class VoltageRelatedEvent(Event):
-
-    class Meta:
-        abstract = True
-
-    phase_a = models.FloatField(default=0)
-    phase_b = models.FloatField(default=0)
-    phase_c = models.FloatField(default=0)
-
-    def save_event(self, measurement):
-        event = self.__class__()
-        event.phase_a = measurement.voltage_a
-        event.phase_b = measurement.voltage_b
-        event.phase_c = measurement.voltage_c
-
-        event.save()
-        return event
-
-
 class FailedConnectionEvent(Event):
     """
     Defines a new event related to a failed connection with a transductor
     """
-    transductor_ip = models.CharField(
-        max_length=15,
-        unique=True,
-        default="0.0.0.0",
-        validators=[
-            RegexValidator(
-                regex='^(?:[0-9]{1,3}\.){3}[0-9]{1,3}$',
-                message='Incorrect IP address format',
-                code='invalid_ip_address'
-            ),
-        ])
 
-    transductor = models.ForeignKey(
-        EnergyTransductor,
-        related_name="%(app_label)s_%(class)s",
-        on_delete=models.CASCADE,
-        blank=False,
-        null=False
-    )
-
-    @staticmethod
-    def save_event(transductor):
+    def save_event(self, transductor):
         """
         Saves a failed connection event related to a transductor
         """
         new_event = FailedConnectionEvent()
         new_event.transductor = transductor
-        new_event.transductor_ip = new_event.transductor.ip_address
+        new_event.measures = {}
 
         new_event.save()
         return new_event
 
 
-class CriticalVoltageEvent(VoltageRelatedEvent):
+class CriticalVoltageEvent(Event):
     """
     Defines a new event related to a critical voltage measurement
     """
 
+    def save_event(self, transductor, list_critical_phases):
+        new_event = CriticalVoltageEvent()
+        new_event.transductor = transductor
+        new_event.measures = {}
 
-class PrecariousVoltageEvent(VoltageRelatedEvent):
+        for phase in list_critical_phases:
+            new_event.measures[phase[0]] = phase[1]
+
+        new_event.save()
+        return new_event
+
+
+class PrecariousVoltageEvent(Event):
     """
     Defines a new event related to a precarious voltage measurement
     """
 
+    def save_event(self, transductor, list_precarious_phases):
+        new_event = PrecariousVoltageEvent()
+        new_event.transductor = transductor
+        new_event.measures = {}
 
-class PhaseDropEvent(VoltageRelatedEvent):
+        for phase in list_precarious_phases:
+            new_event.measures[phase[0]] = phase[1]
+
+        new_event.save()
+        return new_event
+
+
+class PhaseDropEvent(Event):
     """
     Defines a new event related to a drop on the triphasic voltage measurement
     """
+
+    def save_event(self, transductor, list_down_phases):
+        new_event = PhaseDropEvent()
+        new_event.transductor = transductor
+        new_event.measures = {}
+
+        for phase in list_down_phases:
+            new_event.measures[phase[0]] = phase[1]
+
+        new_event.save()
+        return new_event
