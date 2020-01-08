9909c14a9aba9527ee0b2ea41fe1cfeb96cf3c9b
Guilherme Augusto <guilherme.francais@gmail.com>
2020-01-08 16:51:56 -0300

    Fixes event filters by static date filter
    Co-authored-by: Andre Filho <andre.filho001@outlook.com>

--- events/views.py
+++ events/views.py
@@ -1,21 +1,23 @@
 from rest_framework import viewsets
 from rest_framework import mixins
-from .serializers import EventSerializer
-from .models import Event
-from .models import FailedConnectionEvent
+from .serializers import VoltageRelatedEventSerializer
+from .serializers import FailedConnectionTransductorEventSerializer
+from .models import VoltageRelatedEvent
+from .models import FailedConnectionTransductorEvent
 from .models import CriticalVoltageEvent
 from .models import PrecariousVoltageEvent
 from .models import PhaseDropEvent
+from django.utils import timezone
 
 
-class EventViewSet(mixins.RetrieveModelMixin,
-                   mixins.DestroyModelMixin,
-                   mixins.ListModelMixin,
-                   viewsets.GenericViewSet):
-    serializer_class = EventSerializer
-    queryset = Event.objects.all()
+class VoltageRelatedEventViewSet(mixins.RetrieveModelMixin,
+                                 mixins.DestroyModelMixin,
+                                 mixins.ListModelMixin,
+                                 viewsets.GenericViewSet):
+    serializer_class = VoltageRelatedEventSerializer
+    queryset = VoltageRelatedEvent.objects.all()
     models = {
-        'FailedConnectionEvent': FailedConnectionEvent,
+        'FailedConnectionTransductorEvent': FailedConnectionTransductorEvent,
         'CriticalVoltageEvent': CriticalVoltageEvent,
         'PrecariousVoltageEvent': PrecariousVoltageEvent,
         'PhaseDropEvent': PhaseDropEvent
@@ -23,8 +25,15 @@ class EventViewSet(mixins.RetrieveModelMixin,
 
     def get_queryset(self):
         type = self.request.query_params.get('type')
-        start_date = self.request.query_params.get('start_date')
-        end_date = self.request.query_params.get('end_date')
+        # The period is defined by each minute because the collection for the
+        # measurement related is defined by each minute too.
+        now = timezone.now()
+        start_date = timezone.datetime(
+            now.year, now.month, now.day, now.hour-1, now.minute, 0, 0
+        )
+        end_date = timezone.datetime(
+            now.year, now.month, now.day, now.hour-1, now.minute, 59, 999999
+        )
 
         if start_date and end_date:
             self.queryset = self.queryset.filter(
@@ -38,16 +47,49 @@ class EventViewSet(mixins.RetrieveModelMixin,
         events = []
         for event in self.queryset:
             data = {}
-            print('*'*100)
-            # print(event.measures)
-            data['measures'] = []
+            data['measures'] = {}
             for measure in event.measures.keys():
-                data['measures'].append([measure, event.measures[measure]])
-                # print(data['measures'][measure])
-            # print(data['measures'])
+                data['measures'].setdefault(measure)
+                data['measures'].update({measure: event.measures[measure]})
+
+            data['ip_address'] = event.transductor.ip_address
             data['created_at'] = event.created_at
             data['type'] = event.__class__.__name__
-            print(data)
+            events.append(data)
+
+        return events
+
+
+class FailedConnectionTransductorEventViewSet(mixins.RetrieveModelMixin,
+                                   mixins.DestroyModelMixin,
+                                   mixins.ListModelMixin,
+                                   viewsets.GenericViewSet):
+    serializer_class = FailedConnectionTransductorEventSerializer
+    queryset = FailedConnectionTransductorEvent.objects.all()
+
+    def get_queryset(self):
+        type = self.request.query_params.get('type')
+        # The period is defined by each minute because the collection for the
+        # measurement related is defined by each minute too.
+        now = timezone.now()
+        start_date = now - timezone.timedelta(minutes=1)
+        end_date = now
+
+        if start_date and end_date:
+            self.queryset = self.queryset.filter(
+                created_at__gte=start_date,
+                created_at__lte=end_date
+            )
+
+        if type:
+            self.queryset = self.queryset.instance_of(self.models[type])
+
+        events = []
+        for event in self.queryset:
+            data = {}
+            data['ip_address'] = event.transductor.ip_address
+            data['created_at'] = event.created_at
+            data['type'] = event.__class__.__name__
             events.append(data)
 
         return events
