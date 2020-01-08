7363de1360269946f1f8be435bee87bb555cc8d6
Alax Alves <alaxallves@gmail.com>
2020-01-08 11:51:40 -0300

    Updating Django and djangorest, removing boogie and fixing measurement collection hour

--- smi/settings.py
+++ smi/settings.py
@@ -103,14 +103,6 @@ DATABASES = {
     }
 }
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     }
-# }
-
-
 # Password validation
 # https://docs.djangoproject.com/en/2.1/ref/settings/#auth-password-validators
 
@@ -140,9 +132,6 @@ TIME_ZONE = 'America/Sao_Paulo'
 USE_I18N = True
 
 USE_L10N = True
-
-USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
