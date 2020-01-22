4df31e1ab9b3d7374221b7940a5f737e20a3a833
Arthur Assis <arthur120496@gmail.com>
2020-01-22 11:23:04 -0300

    Fixes broker and slave communication

--- broker.py
+++ broker.py
@@ -2,34 +2,125 @@ import socket
 import threading
 import queue
 import pickle
+import time
+
+# message from slave format
+# {
+#     'protocol':
+#     'ip':
+#     'port':
+#     'content':
+# }
+
+# response format
+# {
+#     'status':
+#     'content'
+# }
 
 
-# [ip, protocolo, sender, message]
+MSG_SIZE = 1024
 
-def worker(q):
-    print(q)
+
+
+def send_udp_messages(messages, address, response=True):
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s.settimeout(1)
+    for message in messages:
+        send_udp_message(message, address, s, response)
+    return response
+
+def send_udp_message(message, address, s = None, response=True):
+    if(s is None):
+        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        s.settimeout(1)
+    s.sendto(message, address)
+    if(response):
+        response, transductor = s.recvfrom(MSG_SIZE)
+
+        return response
+
+def send_tcp_messages(messages, address):
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.settimeout(3)
+    s.connect(address)
+    print("Here2")
+    responses = []
+    for message in messages:
+        print(pickle.dumps(message))
+        s.send(message)
+        response, transductor = s.recvfrom(MSG_SIZE)
+        print("Here2.5")
+        print(pickle.dumps(response))
+        responses.append(response)
+    s.close()
+    return responses
+
+def worker(message_queue):
     while(True):
-        r = q.get()
-        print(r)
+        aux = message_queue.get()
+        print("Here7")
+        print(aux)
+        message = aux[0]
+        sender = aux[1]
+        print("Here1.5")
+        print(message)
+        transductor = (message['ip'], message['port'])
+        try:
+            if(message['protocol'] == 'UDP'):
+                transductor_response = send_udp_message(
+                    message['content'], transductor)
+                response = {}
+                response['status'] = 1
+                response['content'] = transductor_response
+            elif(message['protocol'] == 'TCP'):
+                transductor_response = send_tcp_message(
+                    message['content'], transductor)
+                response = {}
+                response['status'] = 1
+                response['content'] = transductor_response
+            else:
+                response['status'] = 0
+                response['content'] = 'Unknown protocol'
+        except Exception as e:
+            response = {}
+            response['status'] = 0
+            response['content'] = e
+        print("Here3")
+        print(response)
+        print(sender)
+        response = pickle.dumps(response)
+        try:
+            send_udp_message(response, sender,response=False) 
+        except:
+            pass
+        print("Here4")
+        print(response)
 
 if __name__ == '__main__':
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    s.bind(('localhost', 1996))
-    q = {} 
+    s.bind(('0.0.0.0', 6071))
+
+    # This is a dict with all the message queues that uses the ip of 
+    # the reciver as key and the queue of messages to that reciver as value
+    message_queues = {}
+
     while(True):
-        r, a = s.recvfrom(1024)
-        r = pickle.loads(r)
-        print(r)
-        if(q.__contains__(r['ip'])):
-            print(1)
-            ip = q[r['ip']]
-            ip.add(r)
+        message, sender = s.recvfrom(MSG_SIZE)
+        message = pickle.loads(message)
+        print("Here1")
+        print(message)
+        queue_identifier = message['ip']
+
+        if(message_queues.__contains__(message['ip'])):
+            ip = message_queues[queue_identifier]
+            ip.put([message, sender])
 
         else:
-            print(2)
-            q[r['ip']] = queue.Queue()
-            q[r['ip']].put(r)
-            t = threading.Thread(target=worker,args=(q[r['ip']],))
-            t.start()
-        
+            message_queues[message['ip']] = queue.Queue()
 
+            message_queues[message['ip']].put([message, sender])
+
+            t = threading.Thread(target=worker, args=(
+                message_queues[message['ip']],))
+            t.start()
--- data_reader/transport.py
+++ data_reader/transport.py
@@ -1,5 +1,6 @@
 import importlib
 import socket
+import pickle
 
 from abc import ABCMeta, abstractmethod
 
@@ -21,11 +22,10 @@ class TransportProtocol(metaclass=ABCMeta):
         socket (socket._socketobject): The socket used in communication.
     """
 
-    def __init__(self, serial_protocol, timeout=2, port=1001):
+    def __init__(self, serial_protocol, timeout=20):
         self.serial_protocol = serial_protocol
         self.transductor = serial_protocol.transductor
         self.timeout = timeout
-        self.port = port
         self.socket = None
 
     def send_messages(self, messages):
@@ -102,12 +102,76 @@ class UdpProtocol(TransportProtocol):
 
 class TcpProtocol(TransportProtocol):
 
+    # def open_socket(self):
+    #     self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    #     self.socket.settimeout(1)
+    #     self.socket.connect((self.transductor.ip_address, 1001))
+
     def open_socket(self):
-        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.socket.settimeout(1)
-        self.socket.connect((self.transductor.ip_address, 1001))
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.receive_attempts = 0
+        self.max_receive_attempts = 7
+
+    def send_messages(self, messages):
+        # response_messages = []
+        self.open_socket()
+        # for message in messages:
+        # response_messages.append(self.send_message(messages))
+        print(messages)
+        response_messages = self.send_message(messages)
+        # print(response_messages)
+        return response_messages
 
     def send_message(self, message):
-        self.socket.send(message)
-        received_message = self.socket.recvfrom(1024)
-        return received_message[0]
+        receive_attemps = 0
+        crc_errors = 0
+        received_message = None
+        while(
+            not received_message and (
+                receive_attemps < self.max_receive_attempts
+            )
+        ):
+
+            real_message = {}
+            real_message['content'] = message
+            real_message['protocol'] = 'TCP'
+            real_message['ip']= self.transductor.ip_address
+            real_message['port']= self.transductor.port
+            
+            real_message = pickle.dumps(real_message)
+            
+            try:
+                print(real_message)
+                self.socket.sendto(
+                    real_message,
+                    ('slave-broker', 6071)
+                )
+            except:
+                print('*'*100)
+
+
+            try:
+                received_message, broker = self.socket.recvfrom(1024)
+                received_message = pickle.loads(received_message)
+                print(received_message)
+                if(received_message['status'] == 0):
+                    received_message = None
+                    pass
+            except socket.timeout:
+                receive_attemps += 1
+                if(receive_attemps == self.max_receive_attempts):
+                    raise NumberOfAttempsReachedException(
+                        "Maximum attempts reached!")
+                pass
+            except CRCInvalidException as e:
+                crc_errors += 1
+                if(crc_errors == self.max_receive_attempts):
+                    self.socket.close()
+                    raise e
+        return received_message['content']
+
+
+    # def send_message(self, message):
+    #     self.socket.send(message)
+    #     received_message = self.socket.recvfrom(1024)
+    #     return received_message[0]
