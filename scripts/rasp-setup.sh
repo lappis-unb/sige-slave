9172436b30084c211902ab6ea8bbc1d94819b6e6
Alax Alves <alaxallves@gmail.com>
2020-01-23 10:41:52 -0300

    Adding final steps

--- scripts/rasp-setup.sh
+++ scripts/rasp-setup.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
 BRIDGE_FILE=/etc/systemd/network/bridge.network
+REBOOTED=False
+
+if [ $REBOOTED == 'False' ]; then
+
+fi
 
 echo "========== Upgrading pkgs =========="
 
@@ -23,7 +28,7 @@ IPFoward=kernel
 EOF
     cat $BRIDGE_FILE
 else
-    echo "Bridge file already exists..."
+    echo -e "Bridge file already exists...\n"
     echo $BRIDGE_FILE && cat $BRIDGE_FILE
 fi
 
@@ -41,3 +46,32 @@ show_colored_git_branch_in_prompt
 " >> ~/.bashrc
 
 source ~/.bashrc
+
+echo "========== Restarting Network service =========="
+sudo systemctl restart systemd-networkd.service
+
+echo "========== Installing required Docker dependencies =========="
+sudo apt-get install --no-install-recommends \
+                        apt-transport-https \
+                        ca-certificates \
+                        curl \
+                        gnupg2 \
+                        software-properties-common
+
+curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
+
+sudo tee -a /etc/apt/sources.list.d/docker.list > /dev/null << EOF
+deb [arch=armhf] https://download.docker.com/linux/debian buster stable
+EOF
+
+sudo apt-get update
+
+echo "========== Installing Docker =========="
+sudo apt-get install --no-install-recommends docker-ce
+
+sudo reboot
+
+sudo usermod -aG docker pi
+
+echo "========== Installing Docker Compose =========="
+sudo pip install docker-compose
