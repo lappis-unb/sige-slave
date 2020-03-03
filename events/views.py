1af701e432ba51b8206939b5575eaefb4aa1348e
Guilherme Augusto <guilherme.francais@gmail.com>
2020-03-03 03:17:30 -0300

    Modifies event return to get all slave events

--- events/views.py
+++ events/views.py
@@ -8,6 +8,7 @@ from .models import FailedConnectionTransductorEvent
 from .models import CriticalVoltageEvent
 from .models import PrecariousVoltageEvent
 from .models import PhaseDropEvent
+from transductor.models import EnergyTransductor
 from django.utils import timezone
 
 
@@ -18,7 +19,6 @@ class VoltageRelatedEventViewSet(mixins.RetrieveModelMixin,
     serializer_class = VoltageRelatedEventSerializer
     queryset = VoltageRelatedEvent.objects.none()
     models = {
-        'FailedConnectionTransductorEvent': FailedConnectionTransductorEvent,
         'CriticalVoltageEvent': CriticalVoltageEvent,
         'PrecariousVoltageEvent': PrecariousVoltageEvent,
         'PhaseDropEvent': PhaseDropEvent
@@ -30,23 +30,24 @@ class VoltageRelatedEventViewSet(mixins.RetrieveModelMixin,
         types = list(self.models.keys())
 
         events = []
-        for type in types:
-            last_event = VoltageRelatedEvent.objects.instance_of(
-                self.models[type]
-            ).last()
+        for transductor in EnergyTransductor.objects.all():
+            for type in types:
+                last_event = transductor.events_event.instance_of(
+                    self.models[type]
+                ).last()
 
-            if last_event:
-                data = {}
-                data['data'] = {}
-                for measure in last_event.data.keys():
-                    data['data'].setdefault(measure)
-                    data['data'].update({measure: last_event.data[measure]})
+                if last_event:
+                    data = {}
+                    data['data'] = {}
+                    for measure in last_event.data.keys():
+                        data['data'][measure] = last_event.data[measure]
 
-                data['ip_address'] = last_event.transductor.ip_address
-                data['created_at'] = last_event.created_at
-                data['ended_at'] = last_event.ended_at
-                data['type'] = last_event.__class__.__name__
-                events.append(data)
+                    data['ip_address'] = last_event.transductor.ip_address
+                    data['created_at'] = last_event.created_at
+                    data['ended_at'] = last_event.ended_at
+                    data['type'] = last_event.__class__.__name__
+                    events.append(data)
+                    print('h'*100)
 
         return Response(events, status=200)
 
@@ -56,21 +57,25 @@ class FailedConnectionTransductorEventViewSet(mixins.RetrieveModelMixin,
                                               mixins.ListModelMixin,
                                               viewsets.GenericViewSet):
     serializer_class = FailedConnectionTransductorEventSerializer
-    query = FailedConnectionTransductorEvent.objects.all()
+    queryset = FailedConnectionTransductorEvent.objects.none()
 
     def list(self, request):
-        last_event = None
+        events = []
         # The period is defined by each minute because the collection for the
         # measurement related is defined by each minute too.
 
-        last_event = self.query.last()
+        for transductor in EnergyTransductor.objects.all():
+            last_event = transductor.events_event.instance_of(
+                FailedConnectionTransductorEvent
+            ).last()
 
-        data = {}
-        if last_event:
-            data['data'] = last_event.data
-            data['ip_address'] = last_event.transductor.ip_address
-            data['created_at'] = last_event.created_at
-            data['ended_at'] = last_event.ended_at
-            data['type'] = last_event.__class__.__name__
+            if last_event:
+                data = {}
+                data['data'] = last_event.data
+                data['ip_address'] = last_event.transductor.ip_address
+                data['created_at'] = last_event.created_at
+                data['ended_at'] = last_event.ended_at
+                data['type'] = last_event.__class__.__name__
+                events.append(data)
 
-        return Response(data, status=200)
+        return Response(events, status=200)
