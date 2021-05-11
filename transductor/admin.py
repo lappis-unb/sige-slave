70a437b5e01939398826b459b786a29d72b31cf3
Durval Carvalho <dudurval2@gmail.com>
2021-05-11 20:39:35 -0300

    Add TransductorVoltageState model to admin painel

--- transductor/admin.py
+++ transductor/admin.py
@@ -1,5 +1,15 @@
 from django.contrib import admin
 
-from .models import EnergyTransductor
+from .models import EnergyTransductor, TimeInterval, TransductorVoltageState
+
+
+@admin.register(TransductorVoltageState)
+class TransductorVoltageStateAdmin(admin.ModelAdmin):
+    list_display = (
+        'transductor',
+        'current_voltage_state',
+        'phase',
+    )
 
 admin.site.register(EnergyTransductor)
+admin.site.register(TimeInterval)
