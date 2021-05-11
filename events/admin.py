7f0a78f2373859d04d2c7a7e3180bfe08e6894be
Durval Carvalho <dudurval2@gmail.com>
2021-05-11 20:38:06 -0300

    Add Event model to admin painel

--- events/admin.py
+++ events/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import (Event,
+                    CriticalVoltageEvent,
+                    PrecariousVoltageEvent,
+                    PhaseDropEvent)
+
+
+@admin.register(Event)
+class EventAdmin(admin.ModelAdmin):
+    list_display = (
+        '__str__',
+        'created_at',
+        'ended_at',
+        'data',
+    )
+    list_filter = ('ended_at',)
+
+
+admin.site.register(CriticalVoltageEvent)
+admin.site.register(PrecariousVoltageEvent)
+admin.site.register(PhaseDropEvent)
