7b42899abc4e541852b4a08fdb491a6933d60365
Davi Antônio da Silva Santos <antoniossdavi@gmail.com>
2022-09-14 22:23:35 -0300

    refactor: adiciona tipos e corrige temporização
    O arquivo com as classes para execução dos cronjobs foi alterada para
    melhor adequação aos princípios do Clean Code.
    
    Os métodos, atributos e classes tiveram seus tipos especificados para
    melhor documentação do código.
    
    As medições que são executadas de minuto em minuto eram especificadas
    com o parâmetro zero com um comentário explicando o comportamento
    esperado. Para melhorar a compreensão, a temporização foi substituída
    para explicitamente um minuto em todos os métodos que devem ser
    executados com esse intervalo.
    
    A temporização de quarto de hora, erroneamente especificada em zero
    minutos, e a temporização de 30 dias, também erroneamente especificada
    em 12 horas foram corrigidos.
    
    Vários comentários não significativos foram removidos.
    
    - especifica explicitamente tarefas que executam de minuto em minuto
    - remover código morto comentado
    - adiciona tipos dos métodos
    - corrige tempo de 30 dias
    - corrige tempo de quarto de hora

--- data_reader/cronjob.py
+++ data_reader/cronjob.py
@@ -1,59 +1,64 @@
 from django_cron import CronJobBase, Schedule
 
-# from .utils import perform_all_data_collection, perform_all_data_rescue
 from .utils import perform_all_data_rescue
 from modbus_reader.main import perform_all_data_collection
 
 
 class MinutelyCollectCronJob(CronJobBase):
-    RUN_EVERY_MINS = 0  # 1 in 1 minute
-    schedule = Schedule(run_every_mins=RUN_EVERY_MINS)
-    code = "data_reader.cronjob.MinutelyCollectCronJob"
+    RUN_EVERY_MINS: int = 1
+    schedule: Schedule = Schedule(run_every_mins=RUN_EVERY_MINS)
+    code: str = "data_reader.cronjob.MinutelyCollectCronJob"
+    collection_type: str = "Minutely"
 
-    def do(self):
-        perform_all_data_collection("Minutely")
+    def do(self) -> None:
+        perform_all_data_collection(self.collection_type)
 
 
 class QuarterlyCollectCronJob(CronJobBase):
-    RUN_EVERY_MINS = 15
-    schedule = Schedule(run_every_mins=RUN_EVERY_MINS)
-    code = "data_reader.cronjob.QuarterlyCollectCronJob"
+    RUN_EVERY_MINS: int = 15
+    schedule: Schedule = Schedule(run_every_mins=RUN_EVERY_MINS)
+    code: str = "data_reader.cronjob.QuarterlyCollectCronJob"
+    collection_type: str = "Quarterly"
 
-    def do(self):
-        perform_all_data_collection("Quarterly")
+    def do(self) -> None:
+        perform_all_data_collection(self.collection_type)
 
 
 class MonthlyCollectCronJob(CronJobBase):
-    RUN_EVERY_MINS = 720  # 30 in 30 days
-    schedule = Schedule(run_every_mins=RUN_EVERY_MINS)
-    code = "data_reader.cronjob.MonthlyCollectCronJob"
+    RUN_EVERY_MINS: int = 30 * 24 * 60
+    schedule: Schedule = Schedule(run_every_mins=RUN_EVERY_MINS)
+    code: str = "data_reader.cronjob.MonthlyCollectCronJob"
+    collection_type: str = "Monthly"
 
-    def do(self):
-        perform_all_data_collection("Monthly")
+    def do(self) -> None:
+        perform_all_data_collection(self.collection_type)
 
 
 class MinutelyDataRescueCronJob(CronJobBase):
-    RUN_EVERY_MINS = 0  # 1 in 1 minute
-    schedule = Schedule(run_every_mins=RUN_EVERY_MINS)
-    code = "data_reader.cronjob.MinutelyDataRescueCronJob"
+    RUN_EVERY_MINS: int = 1
+    schedule: Schedule = Schedule(run_every_mins=RUN_EVERY_MINS)
+    code: str = "data_reader.cronjob.MinutelyDataRescueCronJob"
+    collection_type: str = "Minutely"
 
-    def do(self):
-        perform_all_data_rescue("Minutely")
+    def do(self) -> None:
+        perform_all_data_rescue(self.collection_type)
 
 
 class QuarterlyDataRescueCronJob(CronJobBase):
-    RUN_EVERY_MINS = 0  # 15
-    schedule = Schedule(run_every_mins=RUN_EVERY_MINS)
-    code = "data_reader.cronjob.QuarterlyDataRescueCronJob"
+    RUN_EVERY_MINS: int = 15
+    schedule: Schedule = Schedule(run_every_mins=RUN_EVERY_MINS)
+    code: str = "data_reader.cronjob.QuarterlyDataRescueCronJob"
+    collection_type: str = "Quarterly"
 
-    def do(self):
-        perform_all_data_rescue("Quarterly")
+    def do(self) -> None:
+        perform_all_data_rescue(self.collection_type)
 
 
 class MonthlyDataRescueCronJob(CronJobBase):
-    RUN_EVERY_MINS = 720  # 30 in 30 days
-    schedule = Schedule(run_every_mins=RUN_EVERY_MINS)
-    code = "data_reader.cronjob.MonthlyDataRescueCronJob"
+    RUN_EVERY_MINS: int = 30 * 24 * 60
+    schedule: Schedule = Schedule(run_every_mins=RUN_EVERY_MINS)
+    code: str = "data_reader.cronjob.MonthlyDataRescueCronJob"
+    collection_type: str = "Monthly"
 
-    def do(self):
-        perform_all_data_rescue("Monthly")
+    def do(self) -> None:
+        perform_all_data_rescue(self.collection_type)
