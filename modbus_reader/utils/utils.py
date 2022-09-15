3e029fa7bf0aab8a6d322893e2659c37fbcef36c
Davi Antônio da Silva Santos <antoniossdavi@gmail.com>
2022-09-15 14:38:16 -0300

    style(modbus_reader/utils/utils.py): formata com Black
    O arquivo `modbus_reader/utils/utils.py` foi reformatado pelo Black
    22.3.0.

--- modbus_reader/utils/utils.py
+++ modbus_reader/utils/utils.py
@@ -31,7 +31,7 @@ class ModbusTypeDecoder(object):
             "bits": self.parse_bits(),
         }
 
-# These methods create utility functions
+    # These methods create utility functions
     @staticmethod
     def parse_bits():
         """
@@ -121,16 +121,18 @@ def load_handler(path: str, *args, **kwargs):
     """
     Given a path to a handler, return an instance of that handler.
     """
-    i: int = path.rfind('.')
+    i: int = path.rfind(".")
     module: str
     attr: str
-    module, attr = path[:i], path[i+1:]
+    module, attr = path[:i], path[i + 1 :]
     try:
         mod: ModuleType = import_module(module)
         cls = getattr(mod, attr)
 
     except AttributeError as e:
-        raise ParameterException('invalid parameter (Module: "%s" or attr: "%s")' % (module, attr)) from e
+        raise ParameterException(
+            'invalid parameter (Module: "%s" or attr: "%s")' % (module, attr)
+        ) from e
 
     return cls(*args, **kwargs)
 
@@ -158,7 +160,7 @@ def remove_format_datetime(measurements):
         measurements_copy["day_of_the_month"],
         measurements_copy["hour"],
         measurements_copy["minute"],
-        measurements_copy["second"]
+        measurements_copy["second"],
     )
 
 
@@ -177,16 +179,49 @@ def type_modbus(data_type: str) -> str:
     """
 
     clean_format: str = data_type.replace(" ", "").strip("-").lower()
-    if clean_format in {"u8", "ui8", "uint8", }:
+    if clean_format in {
+        "u8",
+        "ui8",
+        "uint8",
+    }:
         data_type = "uint8"
 
-    elif clean_format in {"u16", "ui16", "uint", "word", "uint16", "uint16bit", "unsignedint16bit", "s16", "short", "short16"}:
+    elif clean_format in {
+        "u16",
+        "ui16",
+        "uint",
+        "word",
+        "uint16",
+        "uint16bit",
+        "unsignedint16bit",
+        "s16",
+        "short",
+        "short16",
+    }:
         data_type = "uint16"
 
-    elif clean_format in {"ui32", "u32", "uint32", "dword", "unsignedint32bit", "ul32", "ulong", "ulong32", "longint32"}:
+    elif clean_format in {
+        "ui32",
+        "u32",
+        "uint32",
+        "dword",
+        "unsignedint32bit",
+        "ul32",
+        "ulong",
+        "ulong32",
+        "longint32",
+    }:
         data_type = "uint32"
 
-    elif clean_format in {"uint64", "u64", "ui64", "ull", "ulong64", "ulonglong", "ulonglong64"}:
+    elif clean_format in {
+        "uint64",
+        "u64",
+        "ui64",
+        "ull",
+        "ulong64",
+        "ulonglong",
+        "ulonglong64",
+    }:
         data_type = "uint64"
 
     elif clean_format in {"int8", "i8", "integer8"}:
@@ -195,25 +230,65 @@ def type_modbus(data_type: str) -> str:
     elif clean_format in {"int", "i16", "int16", "int16bit", "integer", "integer16"}:
         data_type = "int16"
 
-    elif clean_format in {"int32", "i32", "integer32", "l32", "long", "long32", "li32", "longint", "longint32"}:
+    elif clean_format in {
+        "int32",
+        "i32",
+        "integer32",
+        "l32",
+        "long",
+        "long32",
+        "li32",
+        "longint",
+        "longint32",
+    }:
         data_type = "int32"
 
-    elif clean_format in {"int64", "i64", "unixdatastamp", "integer64", "longlong", "longint64"}:
+    elif clean_format in {
+        "int64",
+        "i64",
+        "unixdatastamp",
+        "integer64",
+        "longlong",
+        "longint64",
+    }:
         data_type = "int64"
 
     # Half-precision floating-point format (2 bytes)
-    elif clean_format in {"float16", "f16", "fp16","half"}:
+    elif clean_format in {"float16", "f16", "fp16", "half"}:
         data_type = "float16"
 
     # IEEE 754 single-precision binary floating-point format (4 bytes)
-    elif clean_format in {"f32", "fp32", "float", "float32", "real", "single", "ieee32bitfloatpoint", "ieee32bitfp", "ieee32bit"}:
+    elif clean_format in {
+        "f32",
+        "fp32",
+        "float",
+        "float32",
+        "real",
+        "single",
+        "ieee32bitfloatpoint",
+        "ieee32bitfp",
+        "ieee32bit",
+    }:
         data_type = "float32"
 
     # IEEE 754 double-precision binary floating-point format (8 bytes)
-    elif clean_format in {"f64", "fp64", "float64", "lreal", "double", "ieee64bitfloatpoint", "ieee64bitfp", "ieee64bit"}:
+    elif clean_format in {
+        "f64",
+        "fp64",
+        "float64",
+        "lreal",
+        "double",
+        "ieee64bitfloatpoint",
+        "ieee64bitfp",
+        "ieee64bit",
+    }:
         data_type = "float64"
 
-    elif clean_format in {"bit", "bits", "b", }:
+    elif clean_format in {
+        "bit",
+        "bits",
+        "b",
+    }:
         data_type = "bits"
 
     else:
