7c35d92d808f039813d0a64f96c7848002763aee
lbvx <lucasbraunxav@gmail.com>
2023-11-04 19:56:47 -0300

    Corrige testes do módulo events
    Co-authored-by: Arthur Taylor <arthur_taylor2001@hotmail.com>

--- events/tests/test_event_model.py
+++ events/tests/test_event_model.py
@@ -4,13 +4,69 @@ from django.test import TestCase
 
 from events.models import Event, FailedConnectionTransductorEvent
 from transductor.models import Transductor
+from data_collector.models import MemoryMap
 
 
 class EventTestCase(TestCase):
     def setUp(self):
+        minutely = [
+                {
+                    "size": 6,
+                    "type": "float32",
+                    "function": "read_input_register",
+                    "byteorder": "f2_f1_f0_exp",
+                    "attributes": [
+                        "voltage_a",
+                        "voltage_b",
+                        "voltage_c"
+                    ],
+                    "start_address": 10
+                }
+        ]
+        quarterly = [
+            {
+                    "size": 8,
+                    "type": "float32",
+                    "function": "read_input_register",
+                    "byteorder": "f2_f1_f0_exp",
+                    "attributes": [
+                        "active_consumption",
+                        "reactive_inductive",
+                        "active_generated",
+                        "reactive_capacitive"
+                    ],
+                    "start_address": 200
+                }
+        ]
+        monthly = [
+                {
+                    "size": 8,
+                    "type": "float32",
+                    "function": "read_input_register",
+                    "byteorder": "f2_f1_f0_exp",
+                    "attributes": [
+                        "active_consumption",
+                        "reactive_inductive",
+                        "active_generated",
+                        "reactive_capacitive"
+                    ],
+                    "start_address": 200
+                }
+        ]
+
+        self.memory_map = MemoryMap.objects.create(
+            id=1,
+            model_transductor='TR4020',
+            minutely=minutely,
+            quarterly=quarterly,
+            monthly=monthly
+        )
+
         self.transductor = Transductor.objects.create(
+            id=1,
             serial_number="8764321",
             ip_address="111.101.111.11",
+            port='1234',
             broken=False,
             active=True,
             model="TR4020",
@@ -19,10 +75,13 @@ class EventTestCase(TestCase):
             geolocation_longitude=-24.4556,
             geolocation_latitude=-24.45996,
             installation_date=datetime.now(),
+            memory_map=self.memory_map
         )
         self.transductor2 = Transductor.objects.create(
+            id=2,
             serial_number="8764322",
             ip_address="111.101.111.12",
+            port='1234',
             broken=False,
             active=True,
             model="TR4020",
@@ -31,6 +90,7 @@ class EventTestCase(TestCase):
             geolocation_longitude=-24.4557,
             geolocation_latitude=-24.45997,
             installation_date=datetime.now(),
+            memory_map=self.memory_map
         )
 
     def test_event_behavior(self):
